9d5ffcdd981f02b23e9497f5162a6d4cbddc10d3
Devin Mullin <devin.mullin90@gmail.com>
2021-11-02 09:28:11 -0500

    commit tree 53b05d5cbbcb04c8b4f70fca4e9c14f6f0517325

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,22 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: {
+        id: cheese.id,
+        name: cheese.name,
+        price: cheese.price,
+        is_best_seller: cheese.is_best_seller,
+      }
+    else
+      render json: { error: "cheese not freakin found" }, status: :not_found
+    end
   end
 
 end
